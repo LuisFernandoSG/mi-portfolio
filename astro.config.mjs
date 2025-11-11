bafbf66ff8d555a17b146373c34e775168ba6b8f
Luis Fernando <lfsalguerog@gmail.com>
2025-11-10 19:31:23 -0600

    Configuracion vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
+  adapter: vercel({
+      // Puedes usar 'serverless' o 'edge'
+      // 'serverless' es más común y fiable
+      // mode: 'serverless' 
+  }),
 
   integrations: [react()]
 });
